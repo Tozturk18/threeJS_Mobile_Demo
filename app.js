85ee2f3621bbc2187ad432adad69985d149e2e3e
tozturk18 <tunaozturk2001@hotmail.com>
2022-12-07 21:35:46 +0300

    OMG I DID ITTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import * as THREE from 'https://unpkg.com/three@0.138.0/build/three.module.js';
-//import { OrbitControls } from 'OrbitControls';
+import { OrbitControls } from 'https://unpkg.com/three@0.138.0/examples/jsm/controls/OrbitControls.js';
 
     console.log("Hello2");
 
@@ -9,7 +9,6 @@ import * as THREE from 'https://unpkg.com/three@0.138.0/build/three.module.js';
     renderer.setSize( document.body.clientWidth, document.body.clientHeight ); // Renderer Aspect Ratio
     renderer.shadowMap.enabled = true; // Renderer Shadow options
     renderer.shadowMap.type = THREE.PCFSoftShadowMap; // default THREE.PCFShadowMap
-    document.querySelector("main").innerHTML = renderer;
     document.querySelector("main").appendChild( renderer.domElement ); // Instantiate the Renderer on the Webpage
     
 
@@ -25,7 +24,7 @@ import * as THREE from 'https://unpkg.com/three@0.138.0/build/three.module.js';
     camera.position.z = 5*Math.cos(Math.PI/6);
     camera.position.y = 5*Math.sin(Math.PI/6);
     // Create an Orbital Camera Controls
-    //const controls = new OrbitControls(camera, renderer.domElement);
+    const controls = new OrbitControls(camera, renderer.domElement);
 
     const cube = new THREE.Mesh( new THREE.BoxGeometry(3,3,3), new THREE.MeshBasicMaterial({color: 0xff0000,}));
 
