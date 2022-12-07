efd8fa8d97ea97f9e6952f40382e0a26ca594afe
tozturk18 <tunaozturk2001@hotmail.com>
2022-12-07 21:23:03 +0300

    step4

--- new.js
+++ new.js
@@ -1,3 +1,5 @@
+import * as THREE from 'three';
+
 document.body.querySelector("main").innerHTML = "";
 document.body.querySelector("main").innerHTML += "HELLOOO!";
 document.body.querySelector("main").innerHTML += " MY NAME IS TUNA";
