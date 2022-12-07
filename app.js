7b746ba7b3dd93235f31c8f63c73f2b68e6dfe1c
tozturk18 <tunaozturk2001@hotmail.com>
2022-12-07 18:32:01 +0300

    tried the loader function trick

--- app.js
+++ app.js
@@ -1,41 +1,45 @@
-"use strict";
-
 import * as THREE from 'three';
 import { OrbitControls } from 'OrbitControls';
 
-// Create a THREE JS WebGL Renderer
-const renderer = new THREE.WebGLRenderer({ antialias: true } );
-// Renderer Settings
-renderer.setSize( document.body.clientWidth, document.body.clientHeight ); // Renderer Aspect Ratio
-renderer.shadowMap.enabled = true; // Renderer Shadow options
-renderer.shadowMap.type = THREE.PCFSoftShadowMap; // default THREE.PCFShadowMap
-document.querySelector("main").appendChild( renderer.domElement ); // Instantiate the Renderer on the Webpage
+function loader() {
 
-// Create a THREE JS Scene
-const scene = new THREE.Scene();
-// Create a THREE JS Perspective Camera with 75 FOV
-const camera = new THREE.PerspectiveCamera( 75, document.body.clientWidth / document.body.clientHeight, 0.1, 1000 );
-// Instantiate the Camera in the scene
-scene.add( camera );
-// Set the initial Camera Position so the Northern Hemisphere is in focus
-camera.position.z = 5*Math.cos(Math.PI/6);
-camera.position.y = 5*Math.sin(Math.PI/6);
-// Create an Orbital Camera Controls
-const controls = new OrbitControls(camera, renderer.domElement);
+    // Create a THREE JS WebGL Renderer
+    const renderer = new THREE.WebGLRenderer({ antialias: true } );
+    // Renderer Settings
+    renderer.setSize( document.body.clientWidth, document.body.clientHeight ); // Renderer Aspect Ratio
+    renderer.shadowMap.enabled = true; // Renderer Shadow options
+    renderer.shadowMap.type = THREE.PCFSoftShadowMap; // default THREE.PCFShadowMap
+    document.querySelector("main").appendChild( renderer.domElement ); // Instantiate the Renderer on the Webpage
 
-const cube = new THREE.Mesh( new THREE.BoxGeometry(3,3,3), new THREE.MeshBasicMaterial({color: 0xff0000,}));
+    // Create a THREE JS Scene
+    const scene = new THREE.Scene();
+    // Create a THREE JS Perspective Camera with 75 FOV
+    const camera = new THREE.PerspectiveCamera( 75, document.body.clientWidth / document.body.clientHeight, 0.1, 1000 );
+    // Instantiate the Camera in the scene
+    scene.add( camera );
+    // Set the initial Camera Position so the Northern Hemisphere is in focus
+    camera.position.z = 5*Math.cos(Math.PI/6);
+    camera.position.y = 5*Math.sin(Math.PI/6);
+    // Create an Orbital Camera Controls
+    const controls = new OrbitControls(camera, renderer.domElement);
 
-scene.add(cube);
+    const cube = new THREE.Mesh( new THREE.BoxGeometry(3,3,3), new THREE.MeshBasicMaterial({color: 0xff0000,}));
 
-function animate() {
-    requestAnimationFrame( animate );
+    scene.add(cube);
+
+    function animate() {
+        requestAnimationFrame( animate );
+    
+        // Update the controls for the Orbital Camera to rotate automatically
+        //controls.update();
+    
+        // Rerender the scene
+        renderer.render(scene, camera);
+    }
   
-    // Update the controls for the Orbital Camera to rotate automatically
-    //controls.update();
-  
-    // Rerender the scene
-    renderer.render(scene, camera);
-  }
-  
-  // Call the animate() function
-  animate();
+    // Call the animate() function
+    animate();
+
+}
+
+window.onload = loader;
