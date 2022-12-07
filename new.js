db39b5301684c37ce3736b0d4588641e08a27bfa
tozturk18 <tunaozturk2001@hotmail.com>
2022-12-07 22:11:17 +0300

    pls

--- new.js
+++ new.js
@@ -1,11 +1,2 @@
-import * as THREE from 'three';
-
-document.body.querySelector("main").innerHTML = "";
-document.body.querySelector("main").innerHTML += "HELLOOO!";
-document.body.querySelector("main").innerHTML += " MY NAME IS TUNA";
-
-alert(THREE.Mesh);
-
-
-
-
+import * as THREE from 'https://unpkg.com/three@0.138.0/build/three.module.js';
+import { OrbitControls } from "https://unpkg.com/three@0.138.0/examples/jsm/controls/OrbitControls.js";
