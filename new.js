0ca67b8beca22879616a0c706bf028afeee122a7
tozturk18 <tunaozturk2001@hotmail.com>
2022-12-07 20:25:05 +0300

    oh god help me

--- new.js
+++ new.js
@@ -8,6 +8,7 @@ console.log("Hello");
 
 console.log(document);
 console.log(document.body);
+console.log(document.querySelector('body'));
 console.log(document.body.querySelector("main"));
 document.body.querySelector('main');
 document.body.querySelector("main").innerHTML = "HELLOOO!";
