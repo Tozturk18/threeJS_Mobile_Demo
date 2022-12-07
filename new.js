77706d42beeb2de8eb184af4956cba31632f142f
tozturk18 <tunaozturk2001@hotmail.com>
2022-12-07 20:17:44 +0300

    HELP

--- new.js
+++ new.js
@@ -7,3 +7,6 @@ window.onload = newLoader;
 console.log("Hello");
 
 document.body.querySelector("main").innerHTML = "HELLOOO!";
+console.log(document);
+console.log(document.body);
+console.log(document.body.querySelector("main"));
