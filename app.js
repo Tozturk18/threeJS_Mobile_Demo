afc4ba49a07f7848a3220a35cb9f61907758ba64
tozturk18 <tunaozturk2001@hotmail.com>
2022-12-07 22:11:07 +0300

    GODD!

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-import * as THREE from 'three';
-//import { OrbitControls } from "https://unpkg.com/three@0.138.0/examples/jsm/controls/OrbitControls.js";
+import * as THREE from 'https://unpkg.com/three@0.138.0/build/three.module.js';
+import { OrbitControls } from "https://unpkg.com/three@0.138.0/examples/jsm/controls/OrbitControls.js";
 
     console.log("Hello2");
 
