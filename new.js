2d736e0cf0f7b82ec293ed672ee4a52b9b1a8b30
tozturk18 <tunaozturk2001@hotmail.com>
2022-12-07 22:15:18 +0300

    what

--- new.js
+++ new.js
@@ -1,2 +1,2 @@
-import * as THREE from 'https://unpkg.com/three@0.138.0/build/three.module.js';
+//import * as THREE from 'https://unpkg.com/three@0.138.0/build/three.module.js';
 import { OrbitControls } from "https://unpkg.com/three@0.138.0/examples/jsm/controls/OrbitControls.js";
