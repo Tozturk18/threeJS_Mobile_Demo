aeadfb4467c5423e49d33a988125118067745af1
tozturk18 <tunaozturk2001@hotmail.com>
2022-12-07 21:26:50 +0300

    step5

--- new.js
+++ new.js
@@ -4,6 +4,8 @@ document.body.querySelector("main").innerHTML = "";
 document.body.querySelector("main").innerHTML += "HELLOOO!";
 document.body.querySelector("main").innerHTML += " MY NAME IS TUNA";
 
+alert(THREE);
+
 
 
 
