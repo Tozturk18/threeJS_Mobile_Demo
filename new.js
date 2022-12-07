7013b3fb1b729102197cff8d2e376e4fba795c63
tozturk18 <tunaozturk2001@hotmail.com>
2022-12-07 20:41:30 +0300

    FFFFFFFFFFFFFFF

--- new.js
+++ new.js
@@ -10,5 +10,6 @@ console.log(document.body);
 console.log(document.querySelector('body'));
 console.log(document.body.querySelector("main"));
 document.body.querySelector('main');
+document.body.querySelector("main").innerHTML = "HELLOOO!";
 
 
