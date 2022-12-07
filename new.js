a6d78006725f2a5623871027749f126ed29ded87
tozturk18 <tunaozturk2001@hotmail.com>
2022-12-07 21:14:13 +0300

    ok lets start from the beginning

--- new.js
+++ new.js
@@ -1,15 +1,5 @@
-function newLoader() {
-    console.log("HELLOOO!");
-    document.body.querySelector("main").innerHTML = "HELLOOO!";
-}
-
-window.onload = newLoader;
-
-console.log(document);
-console.log(document.body);
-console.log(document.querySelector('body'));
-console.log(document.body.querySelector("main"));
-document.body.querySelector('main');
 document.body.querySelector("main").innerHTML = "HELLOOO!";
+
+
 
 
