ad4f0ce2322633d9c4d09da1b36498f85361c34d
tozturk18 <tunaozturk2001@hotmail.com>
2022-12-08 22:58:38 +0300

    trying canvas width/height

--- DomMesh.js
+++ DomMesh.js
@@ -153,7 +153,7 @@ class DomMesh extends Mesh {
 
             // calculate pointer position in normalized device coordinates
             // (-1 to +1) for both components
-            const pointer = new Vector2( Math.round( ( ( event.touches[0].screenX / window.innerWidth ) * 2 - 1 )*1000 )/1000, Math.round(( - ( event.touches[0].screenY / window.innerHeight ) * 2 + 1 )*1000)/1000 );
+            const pointer = new Vector2( Math.round( ( ( event.touches[0].clientX / document.querySelector("canvas").width ) * 2 - 1 )*1000 )/1000, Math.round(( - ( event.touches[0].clientY / document.querySelector("canvas").height ) * 2 + 1 )*1000)/1000 );
     
             // update the picking ray with the camera and pointer position
             raycaster.setFromCamera( pointer, camera );
